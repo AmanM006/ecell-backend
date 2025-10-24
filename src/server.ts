15b07ed44a105be848d8d91ac4cce4e8e7379b33
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-10-24 17:18:05 +0530

    Add health check route to monitor server status

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import cors from "cors";
 import express, { type Express } from "express";
 import { pino } from "pino";
 
+import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
+
 const serverLogger = pino({ name: "server" });
 const app: Express = express();
 
@@ -9,5 +11,8 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+// Routes
+app.use("/health-check", healthCheckRouter);
 
 export { app, serverLogger };
