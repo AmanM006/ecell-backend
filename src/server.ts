aa874e18e4be06300b661a5bb047f87d43fd448d
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-10-24 18:01:21 +0530

    Request Logging added with Pino

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,22 @@ import express, { type Express } from "express";
 import { pino } from "pino";
 
 import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
+import { env } from "@/common/utils/envConfig";
+import httpLogger from "@/common/middleware/requestLogger";
 
-const serverLogger = pino({ name: "server" });
+const serverLogger = pino({
+  name: "server",
+  transport: env.isProduction ? undefined : { target: "pino-pretty" },
+});
 const app: Express = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+// Request Logging
+app.use(httpLogger)
 
 // Routes
 app.use("/health-check", healthCheckRouter);
